1123cfe03265255d98b5771226b3ea697b73f0f2
omerbakiciol <weincreative@gmail.com>
2023-03-19 00:29:44 +0300

    Cloud MongoDB connection ve server port eklendi test edildi

--- src/main/java/com/weincreative/ilservice/repository/IlRepository.java
+++ src/main/java/com/weincreative/ilservice/repository/IlRepository.java
@@ -2,10 +2,12 @@ package com.weincreative.ilservice.repository;
 
 import com.weincreative.ilservice.model.Il;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface IlRepository extends MongoRepository<Il, String> {
     List<Il> findAllByName(String name);
     Optional<Il> findByName(String name);
